79cb19260d205defd2e0c929dc8fecaa9b23c662
AleksKon <koneska.aleksandra@gmail.com>
2016-08-05 14:55:48 -0400

    change http to https

--- js/final.js
+++ js/final.js
@@ -201,7 +201,7 @@ function init(){
 
 function loadScript(){
     var script = document.createElement('script');
-    script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init'; //callsback the function created above
+    script.src = 'https://maps.googleapis.com/maps/api/js?sensor=false&callback=init'; //callsback the function created above
     document.body.appendChild(script);
 }
 
